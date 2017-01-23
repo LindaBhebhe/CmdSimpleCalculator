2de1e55eb7bcdafa74cb8cf1bdde01cc47387d2d
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-01-23 10:24:09 +0100

    Have added the code

---
+++ Calc.java
@@ -0,0 +1,28 @@
+
+public class Calc{
+ 
+  public static void main(String[] args){
+  
+  String symbol = args[0];
+  double num1 = Integer.parseInt(args[1]);
+  double num2 = Integer.parseInt(args[2]);
+
+if(symbol.equals("add")){
+  System.out.println();
+  System.out.println(" ***********Simple calculator********");
+System.out.println("The sum is " + (num1 + num2));
+   }
+
+if(symbol.equals("subtract")){
+  System.out.println();
+  System.out.println(" ***********Simple calculator********");
+System.out.println("The difference is " + (num1 - num2));
+   }
+
+if(symbol.equals("multiply")){
+  System.out.println();
+  System.out.println(" ***********Simple calculator********");
+System.out.println("The product is " + (num1 * num2));
+   }
+  }
+}
